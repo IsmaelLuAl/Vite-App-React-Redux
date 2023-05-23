cb72aab2703ee09ebf68dfc2fd97a029014c6792
Ismael Luna <ismaelunalvarez@gmail.com>
2023-05-23 17:53:55 +0200

    React-router-dom dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <SplitList />
+        {/* <SplitList /> */}
       </header>
     </div>
   )
